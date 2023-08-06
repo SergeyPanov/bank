73991584e16e8ffe5916824a9e774103e5ec00bc
SergeyPanov <panovsy@gmail.com>
2023-08-06 11:55:36 +0200

    added login user api

--- api/main_test.go
+++ api/main_test.go
@@ -3,10 +3,26 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/SergeyPanov/bank/db/sqlc"
+	"github.com/SergeyPanov/bank/util"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
--- api/server.go
+++ api/server.go
@@ -1,39 +1,57 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/SergeyPanov/bank/db/sqlc"
+	"github.com/SergeyPanov/bank/token"
+	"github.com/SergeyPanov/bank/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	store      db.Store
+	config     util.Config
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{
-		store: store,
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
 	}
 
-	router := gin.Default()
+	server := &Server{
+		store:      store,
+		config:     config,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccount)
+	server.setupRouter()
 
-	router.POST("/transfers", server.createTransfer)
+	return server, nil
+}
 
-	router.POST("/users", server.createUser)
+func (s *Server) setupRouter() {
+	router := gin.Default()
 
-	server.router = router
+	router.POST("/accounts", s.createAccount)
+	router.GET("/accounts/:id", s.getAccount)
+	router.GET("/accounts", s.listAccount)
 
-	return server
+	router.POST("/transfers", s.createTransfer)
+
+	router.POST("/users", s.createUser)
+	router.POST("/users/login", s.loginUser)
+
+	s.router = router
 }
 
 func (s *Server) Start(address string) error {
